4361f2c3deced0c96e5b1c6b197d055480cd1a05
Corey Webster <corey.webster@figmarketing.com>
2023-08-14 15:43:14 -0400

    added material design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,64 @@ import { AppComponent } from './app.component';
 import { BrowserModule, Title } from '@angular/platform-browser';
 import { HomeComponent } from './feature/home/home.component';
 import { AppRoutingModule } from './app-routing.module';
+import { MatSelectModule } from '@angular/material/select';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatSliderModule } from '@angular/material/slider';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatTooltipModule } from '@angular/material/tooltip'; //matTooltipPosition=[“above”, “below”, “left”, “right”]
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { MatNativeDateModule } from '@angular/material/core';
+import { FormsModule } from '@angular/forms';
 
+const matImports = [
+  MatChipsModule,
+  MatAutocompleteModule,
+  MatButtonModule,
+  MatCheckboxModule,
+  MatDialogModule,
+  MatIconModule,
+  MatInputModule,
+  MatMenuModule,
+  MatRadioModule,
+  MatSelectModule,
+  MatSidenavModule,
+  MatSliderModule,
+  MatSlideToggleModule,
+  MatSnackBarModule,
+  MatToolbarModule,
+  MatTooltipModule,
+  MatDatepickerModule,
+  MatNativeDateModule,
+  MatProgressBarModule,
+  MatProgressSpinnerModule,
+  MatExpansionModule,
+  MatButtonToggleModule,
+  FormsModule
+];
 @NgModule({
-  imports: [AppRoutingModule, BrowserModule],
+  imports: [
+    ...matImports,
+    AppRoutingModule, 
+    BrowserModule,
+    MatSelectModule,
+  ],
   declarations: [AppComponent],
   providers: [Title],
   bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule { }
